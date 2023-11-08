e213e6fe367148a88dcdfb8aa872dec1b237a5c1
rasha fathima <rashafathima325@gmail.com>
2023-11-08 23:44:57 +0530

    Added changes to javascript code for additional functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const newTaskInput = document.querySelector("#wrapper input");
 const taskContainer = document.querySelector("#tasks");
 const error = document.getElementById("error");
 const countValue = document.querySelector(".count-value");
-let taskCount = 0;
+let taskCount = 1;
 
 const displayCount = (taskCount) => {
     countValue.innerText = taskCount;
@@ -24,10 +24,10 @@ const addTask = () => {
             <input type="checkbox" class="task-check">
             <span class="taskname">${taskName}</span>
             <button class="edit">
-                <i class="fa-solid fa-pen-to-square"></i>
+            <i class="fa-regular fa-pen-to-square"></i>
             </button>
             <button class="delete">
-                <i class="fa-solid fa-trash"></i>
+            <i class="fa-solid fa-trash"></i>
             </button>
         </div>
     `;
@@ -42,4 +42,43 @@ const addTask = () => {
             displayCount(taskCount);
         };
     });
+    
+    const editButtons = document.querySelectorAll(".edit");
+    editButtons.forEach((editBtn) => {
+        editBtn.onclick = (e) => {
+            let targetElement = e.target;
+            if (!(e.target.className == "edit")) {
+                targetElement = e.target.parentElement;
+            }
+            newTaskInput.value = targetElement.previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        };
+    });
+    
+    const taskCheck = document.querySelectorAll(".task-check");
+    taskCheck.forEach((checkBox) => {
+        checkBox.onchange = () => {
+            checkBox.nextElementSibling.classList.toggle("completed");
+            if (checkBox.checked) {
+                taskCount -= 1;
+            } else {
+                taskCount += 1;
+            }
+            displayCount(taskCount);
+        };
+    });
+    
+    taskCount += 1;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+};
+
+addBtn.addEventListener("click", addTask);
+
+window.onload = () => {
+    taskCount = 0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
 };
